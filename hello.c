e21f9ccde3ca28a8a87d54b57974b252ff9b9525
Dalton Fischer <dafisch2000@gmail.com>
2019-08-29 12:35:14 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Dalton Fischer
+ * Date: 2019/27/08
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Dalton Fischer\n");
+
+  return 0;
+}
